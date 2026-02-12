f10f80d419eefe74e736c41067c44abdef09bd6b
karimhamed55 <karimhamed2910@gmail.com>
2026-02-12 19:55:20 +0200

    uploading my first api project

---
+++ Controllers/Marketcontroller.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using webAPI.data;
+
+namespace webAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Marketcontroller : ControllerBase
+    {
+        //hold database connection
+        private readonly MarketDbContext _context;
+
+        //constructor to inject database connection
+        public Marketcontroller(MarketDbContext context)
+        {
+            _context = context;
+        }
+
+        //get all products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+        //get one product by id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+        //create a new product
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        }
+        //update a product
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(product).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        //delete a product
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return NoContent();
+
+        }
+    }
+}
---
+++ data/MarketDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace webAPI.data
+{
+    public class MarketDbContext: DbContext
+    {
+        public MarketDbContext(DbContextOptions<MarketDbContext> options): base(options)
+        { } public DbSet<Product> Products { get; set; }
+
+
+    }
+}
